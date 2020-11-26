3159d6dbd2d6bc0887ec27cb54e959a9b43dc4e3
ngo dai <ngothanhdai74@gmail.com>
2020-11-26 21:12:18 +0700

    fd

--- WebApplication2/Controllers/HomeController.cs
+++ WebApplication2/Controllers/HomeController.cs
@@ -5,9 +5,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using WebApplication1.Models;
+using WebApplication2.Models;
 
-namespace WebApplication1.Controllers
+namespace WebApplication2.Controllers
 {
     public class HomeController : Controller
     {
--- WebApplication2/Models/ErrorViewModel.cs
+++ WebApplication2/Models/ErrorViewModel.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace WebApplication1.Models
+namespace WebApplication2.Models
 {
     public class ErrorViewModel
     {
